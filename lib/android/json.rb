abeae54d2878a521909a0d902bab78be59b57e9c
Andrew Havens <email@andrewhavens.com>
2019-03-13 23:34:31 -0700

    Add Object#to_json method on Android.

--- lib/android/json.rb
+++ lib/android/json.rb
@@ -34,3 +34,34 @@ class JSON
     end
   end
 end
+
+class Object
+  def to_json
+    # The Android JSON API expects real Java String objects.
+    @@fix_string ||= (lambda do |obj|
+      case obj
+        when String, Symbol
+          obj = obj.toString
+        when Hash
+          map = Hash.new
+          obj.each do |key, value|
+            key = key.toString if key.is_a?(String) || key.is_a?(Symbol)
+            value = @@fix_string.call(value)
+            map[key] = value
+          end
+          obj = map
+        when Array
+          obj = obj.map do |item|
+            (item.is_a?(String) || item.is_a?(Symbol)) ? item.toString : @@fix_string.call(item)
+          end
+      end
+      obj
+    end)
+
+    obj = Org::JSON::JSONObject.wrap(@@fix_string.call(self))
+    if obj == nil
+      raise "Can't serialize object to JSON"
+    end
+    obj.toString.to_s
+  end
+end
