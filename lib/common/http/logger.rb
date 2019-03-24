d0a023f29b2eb945e1ec84c792a4ced4ca60aa27
Andrew Havens <email@andrewhavens.com>
2019-03-24 15:42:08 -0700

    Improve logger to add color.

--- lib/common/http/logger.rb
+++ lib/common/http/logger.rb
@@ -15,36 +15,60 @@ class Motion
         @enabled = false
       end
 
-      def log(message)
-        puts message if enabled
+      def log(message, color = :white)
+        puts colorize(color, message) if enabled
+      end
+
+      def error(message)
+        puts colorize(:red, message)
       end
 
       def log_request(request)
-        log "Request:\n#{request.http_method.to_s.upcase} #{request.url}"
+        log "\nRequest:\n#{request.http_method.to_s.upcase} #{request.url}", :dark_gray
 
         if request.headers
           request.headers.each do |k,v|
-            log "#{k}: #{v.inspect}"
+            log "#{k}: #{v}", :dark_gray
           end
         end
 
-        if request.params
-          # log serialized_params
-          request.params.each do |k,v|
-            log "\t#{k}=#{v.inspect}"
-          end
-        end
-        log "\n"
+        log(request.body, :dark_gray) if request.body
       end
 
       def log_response(response)
-        log "Response:"
-        log "URL: #{response.original_request.url}"
-        log "Status: #{response.status_code}"
-        response.headers.each do |key, value|
-          log "#{key}: #{value}"
+        log "\nResponse:", :dark_gray
+        log "URL: #{response.original_request.url}", :dark_gray
+        log "Status: #{response.status_code}", :dark_gray
+        response.headers.each do |k,v|
+          log "#{k}: #{v}", :dark_gray
         end
-        log "\n#{response.body}"
+        log "\n#{response.body}", :dark_gray
+      end
+
+      def colorize(color, string)
+        return string unless simulator? # Only colorize in the simulator
+
+        code = {
+          red: 31,
+          dark_gray: 90,
+        }[color]
+
+        if code
+          "\e[#{code}m#{string}\e[0m"
+        else
+          string
+        end
+      end
+
+      # Copied from https://github.com/rubymotion/BubbleWrap/blob/8eaf99a0966f2b375e774f5940279a704c10ad29/motion/core/ios/device.rb#L46
+      def simulator?
+        @simulator_state ||= begin
+          if ios_version.to_i >= 9
+            !NSBundle.mainBundle.bundlePath.start_with?('/var/')
+          else
+            !(UIDevice.currentDevice.model =~ /simulator/i).nil?
+          end
+        end
       end
     end
   end
